961c0069e64380d735624a59b5a7547790603e45
Sun wukong <djesld12q@gmail.com>
2026-04-30 18:53:30 +0300

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,6 @@
+// ═══════════════════════════════════════════════
+//  OneSignal Service Worker
+//  Цей файл МАЄ лежати у корені сайту (поряд з index.html)
+//  Назва файлу незмінна: OneSignalSDKWorker.js
+// ═══════════════════════════════════════════════
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
