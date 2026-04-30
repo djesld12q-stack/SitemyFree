441231b90956de04756c4b37701e09075b313ad1
Sun wukong <djesld12q@gmail.com>
2026-04-30 19:04:22 +0300

    Delete OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++
@@ -1,6 +0,0 @@
-// ═══════════════════════════════════════════════
-//  OneSignal Service Worker
-//  Цей файл МАЄ лежати у корені сайту (поряд з index.html)
-//  Назва файлу незмінна: OneSignalSDKWorker.js
-// ═══════════════════════════════════════════════
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
